abd67a07f9153c5239ac6936fa2bd62704bb9f4d
Gunnar Dahl <gunnardahl4@gmail.com>
2024-02-10 13:22:48 -0600

    designed new start menu scene
    Created a new start menu scene with a background image and a panel with the title and 2 buttons. Hooked the buttons up to the Canvas's StartMenuOnclick script.

---
+++ GatekeeperStudyHall/Assets/Scripts/StartMenuOnclick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleOnclick : MonoBehaviour
+{
+    public void StartGame() {
+        Debug.Log("start pressed");
+    }
+
+    public void QuitGame() {
+        Debug.Log("quit pressed");
+    }
+}
