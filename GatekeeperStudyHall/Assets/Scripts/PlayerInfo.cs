4d93d132e713efb340e67c085f3d108a6edfec8d
Gunnar Dahl <gunnardahl4@gmail.com>
2024-02-14 22:30:37 -0600

    rewrote PlayerInfo
    rewrote the PlayerInfo script to use C# properties instead of private fields, removed clutter and fixed comments

--- GatekeeperStudyHall/Assets/Scripts/PlayerInfo.cs
+++ GatekeeperStudyHall/Assets/Scripts/PlayerInfo.cs
@@ -8,32 +8,13 @@ public class PlayerInfo : MonoBehaviour
         Stores the following information:
 
         Player Card
-        Health - Initialize to 20 HP
-        Stockade - 0 (Player takes no damage when stockade > 1)
+        Health - Initialize to 10 HP
+        Stockades - 0 (Player takes no damage when stockade >= 1)
 
         Maybe TurnOrder / Player Number (Could be handled elsewhere)
     */
 
-    private int health = 20;
-    private int stockade = 0;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    int getHealth(){
-        return health;
-    }
-
-    int getStockade(){
-        return stockade;
-    }
+    // public Card? PlayerCard { get; private set; } // to be implemented
+    public int Health { get; private set; } = 10;
+    public int Stockades { get; private set; } = 0;
 }
