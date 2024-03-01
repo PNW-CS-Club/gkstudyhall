9bc6f28f2ac5d1453dbe7479f0e7b3aa0c24244d
Gunnar Dahl <gunnardahl4@gmail.com>
2024-03-01 17:26:44 -0600

    wrote GameStateMachine
    created a Game State Machine script that keeps track of which part of a turn we are in and lets the state be changed externally

---
+++ GatekeeperStudyHall/Assets/Scripts/GameStateMachine.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public enum GameState
+{
+    ROLLING_TRAIT = 0,
+    CHOOSING_GATE = 1,
+    ATTACKING_GATE = 2,
+    ROLLING_GATE_EFFECT = 3,
+    BATTLING = 4,
+}
+
+
+public class GameStateMachine : MonoBehaviour
+{
+    // this is a property that lets other classes check the game state with `myStateMachine.State`
+    // cannot be set externally due to `private set`
+    public GameState State { get; private set; } = GameState.ROLLING_TRAIT;
+
+    // this is how the state can be changed externally, separated into this function
+    // because there will likely be lots of side effects to handle
+    public void SetState(GameState newState) {
+        State = newState;
+        Debug.Log("The new state is " + newState.ToString());
+    }
+}
