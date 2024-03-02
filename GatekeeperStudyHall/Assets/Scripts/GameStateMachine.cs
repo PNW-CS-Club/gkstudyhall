ea7665a89179bd6e676356ba4bfcc0aed16b4c72
Gunnar Dahl <gunnardahl4@gmail.com>
2024-03-01 18:02:03 -0600

    State machine demo
    made a demo for the state machine stuff with some ui elements.

--- GatekeeperStudyHall/Assets/Scripts/GameStateMachine.cs
+++ GatekeeperStudyHall/Assets/Scripts/GameStateMachine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 [System.Serializable]
@@ -19,12 +20,13 @@ public class GameStateMachine : MonoBehaviour
     // this is a property that lets other classes check the game state with `myStateMachine.State`
     // cannot be set externally due to `private set`
     public GameState State { get; private set; } = GameState.ROLLING_TRAIT;
+    [SerializeField] TMPro.TMP_Text textBox; // temporary, for demo purposes only
 
     // this is how the state can be changed externally, separated into this function
     // because there will likely be lots of side effects to handle
     public void SetState(GameState newState) {
         State = newState;
-        Debug.Log("The new state is " + newState.ToString());
+        textBox.text = "Game State: " + newState.ToString();
     }
     // use this wrapper function to call from an event defined
     // in the editor because enums dont work for some reason
