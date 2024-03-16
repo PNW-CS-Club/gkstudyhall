52694036c64a07f36520a039482b6292b2288eb6
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2024-03-16 10:39:56 -0500

    19 card display (#31)
    * Updated canvas settings
    
    Made the canvas scale to fit the screen properly, set the reference resolution 1920x1080, resized existing ui elements so that it looks like how it did before
    
    * Card prefab
    
    Made a basic prefab for the card, imported dice images
    
    * Used new fonts
    
    Imported some google fonts and made the card use those instead of the ones in the textmeshpro example folder
    
    * Card tweaks
    
    Made a Wolverine card instead of the Harold sample card, changed some of the card prefab settings
    
    * Made cards collapsible
    
    Added a toggle to the DisplayCard script that "collapses" the card, shrinking it and hiding its traits
    
    * Changed outdated comment

--- GatekeeperStudyHall/Assets/Scripts/RollButtonAction.cs
+++ GatekeeperStudyHall/Assets/Scripts/RollButtonAction.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class RollButtonAction : MonoBehaviour
 {
     public void DiceRoll() {
-        // Random.Range returns a float which is then truncated to an int so the range will be 1 - 6
+        // Random.Range(int, int) includes the lower value and excludes the higher value,
+        // so the range will be 1 - 6
         int rollOutcome = Random.Range(1,7);
         Debug.Log(rollOutcome);
     }
