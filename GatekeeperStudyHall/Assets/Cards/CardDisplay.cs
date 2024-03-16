f76a6f6342d1a11aba41062bf5084a07dc122216
Gunnar Dahl <gunnardahl4@gmail.com>
2024-03-16 01:24:33 -0500

    Card tweaks
    Made a Wolverine card instead of the Harold sample card, changed some of the card prefab settings

--- GatekeeperStudyHall/Assets/Cards/CardDisplay.cs
+++ GatekeeperStudyHall/Assets/Cards/CardDisplay.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
@@ -8,6 +9,17 @@ public class CardDisplay : MonoBehaviour
 
     void Start()
     {
-        Debug.Log(cardData.ToString());
+        if (cardData != null) {
+            transform.GetChild(0).GetChild(0).GetComponent<TMPro.TMP_Text>().text = cardData.name;
+
+            transform.GetChild(1).GetComponent<Image>().sprite = cardData.art;
+
+            for (int i = 0; i < 4; i++) {
+                transform.GetChild(2).GetChild(i + 4).GetComponent<TMPro.TMP_Text>().text = cardData.traitNames[i];
+                transform.GetChild(2).GetChild(i + 8).GetComponent<TMPro.TMP_Text>().text = cardData.traitDescriptions[i];
+            }
+
+            transform.GetComponent<Image>().color = cardData.outerColor;
+        }
     }
 }
