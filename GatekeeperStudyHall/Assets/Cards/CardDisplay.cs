8f05594d02acbf0227b10c5762e3ce59556dc39b
Gunnar Dahl <gunnardahl4@gmail.com>
2024-03-16 09:49:42 -0500

    Made cards collapsible
    Added a toggle to the DisplayCard script that "collapses" the card, shrinking it and hiding its traits

--- GatekeeperStudyHall/Assets/Cards/CardDisplay.cs
+++ GatekeeperStudyHall/Assets/Cards/CardDisplay.cs
@@ -7,8 +7,37 @@ public class CardDisplay : MonoBehaviour
 {
     public CardData cardData;
 
+    // if these are different, it means that the checkbox was toggled last frame
+    public bool collapsed;
+    bool wasCollapsed;
+
+    const float COLLAPSE_HEIGHT_DIFF = 172f;
+
     void Start()
     {
+        // start expanded by default
+        wasCollapsed = collapsed;
+        if (collapsed) {
+            Collapse();
+        }
+
+        UpdateDisplay();
+    }
+
+
+    void Update() {
+        if (wasCollapsed != collapsed) {
+            wasCollapsed = collapsed;
+            if (collapsed) {
+                Collapse();
+            } else {
+                Expand();
+            }
+        }
+    }
+
+
+    private void UpdateDisplay() {
         if (cardData != null) {
             transform.GetChild(0).GetChild(0).GetComponent<TMPro.TMP_Text>().text = cardData.name;
 
@@ -21,5 +50,33 @@ public class CardDisplay : MonoBehaviour
 
             transform.GetComponent<Image>().color = cardData.outerColor;
         }
+    }
+
+
+    private void Collapse() {
+        transform.GetChild(2).gameObject.SetActive(false);
+
+        RectTransform rectTransform = transform.GetComponent<RectTransform>();
+        Rect rect = rectTransform.rect;
+        rectTransform.sizeDelta = new Vector2(rect.width, rect.height - COLLAPSE_HEIGHT_DIFF);
+
+        rectTransform.Translate(0f, COLLAPSE_HEIGHT_DIFF / 2f, 0f); 
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).Translate(0f, -COLLAPSE_HEIGHT_DIFF / 2f, 0f);
+        }
+    }
+
+
+    private void Expand() {
+        RectTransform rectTransform = transform.GetComponent<RectTransform>();
+        Rect rect = rectTransform.rect;
+        rectTransform.sizeDelta = new Vector2(rect.width, rect.height + COLLAPSE_HEIGHT_DIFF);
+
+        rectTransform.Translate(0f, -COLLAPSE_HEIGHT_DIFF / 2f, 0f);
+        for (int i = 0; i < transform.childCount; i++) {
+            transform.GetChild(i).Translate(0f, COLLAPSE_HEIGHT_DIFF / 2f, 0f);
+        }
+
+        transform.GetChild(2).gameObject.SetActive(true);
     }
 }
