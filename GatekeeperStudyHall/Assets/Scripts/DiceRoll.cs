16624c21614fef8cee03f4d73b446836741dbbeb
Gunnar Dahl <gunnardahl4@gmail.com>
2024-04-02 16:28:56 -0500

    Better dice animation, bouncy walls on release
    Gave the dice a better animation so that it spins, shakes, and changes face when the mouse is held down. On release it slows down and stops after a set amount of time. When the dice is released, it is teleported in-bounds and some boundary walls turn on.

--- GatekeeperStudyHall/Assets/Scripts/DiceRoll.cs
+++ GatekeeperStudyHall/Assets/Scripts/DiceRoll.cs
@@ -5,31 +5,138 @@ using UnityEngine;
 public class DiceRoll : MonoBehaviour
 {
     bool isRolling = false;
+    bool isFinishing = false;
 
     [SerializeField]
-    float maxRotation = 20f;
+    float maxAngularVel = 720f;
+    [SerializeField]
+    float minAngularVel = 360f;
 
     [SerializeField]
     Sprite[] sprites;
 
     SpriteRenderer spriteRenderer;
+    Rigidbody2D rb;
+
+    float animateTimer = 0f;
+    [SerializeField, Range(0f, 1f)]
+    float animateInterval = 0.1f;
+
+    float finishTimer = 0f;
+    [SerializeField, Range(0f, 2f)]
+    float finishDuration = 0.75f;
+
+    [SerializeField, Range(0, 0.5f)]
+    float frictionFactor = 0.03f;
+
+    [SerializeField]
+    float shakeSpeed = 10f;
+    [SerializeField]
+    float shakeRadius = 1f;
+    [SerializeField]
+    float releaseMultiplier = 5f;
+
+    [SerializeField]
+    Vector2 topLeftBound;
+    [SerializeField]
+    Vector2 bottomRightBound;
+
+    [SerializeField]
+    GameObject barrier;
+
+    int roll = -1;
 
     private void Start() {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        if (sprites.Length != 6) {
+            Debug.LogError($"There should be 6 sprites in DiceRoll array (actual: {sprites.Length})");
+        }
+
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        rb = GetComponentInChildren<Rigidbody2D>();
+        barrier.SetActive(false);
     }
 
     void Update() {
+
         if (isRolling) {
-            transform.localRotation = Quaternion.Euler(0, 0, Random.Range(-maxRotation, maxRotation));
-            spriteRenderer.sprite = sprites[Random.Range(0, 6)];
+
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = transform.position.z;
+            transform.position = mousePosition;
+
+            while (animateTimer >= animateInterval) {
+                spriteRenderer.sprite = sprites[Random.Range(0, 6)];
+
+                float unitCircleAngle = Random.Range(0f, 360f);
+                Vector3 unitCircleVec = new Vector3(Mathf.Cos(unitCircleAngle), Mathf.Sin(unitCircleAngle), 0);
+                Vector3 targetPos = unitCircleVec * shakeRadius;
+                Vector3 newVel = targetPos - rb.transform.localPosition;
+                newVel.z = 0;
+                rb.velocity = newVel.normalized * shakeSpeed;
+
+                float angVel = Random.Range(minAngularVel, maxAngularVel);
+                if (Random.value <= 0.5f) {
+                    angVel = -angVel;
+                }
+                rb.angularVelocity = angVel;
+
+                animateTimer -= animateInterval;
+            }
+
+            animateTimer += Time.deltaTime;
+        }
+
+        else if (isFinishing) {
+
+            rb.angularVelocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
+            rb.velocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
+
+            if (finishTimer >= finishDuration) {
+                rb.angularVelocity = 0;
+                rb.velocity = Vector2.zero;
+                Debug.Log($"You rolled a {roll}.");
+
+                transform.position = rb.transform.position;
+                rb.transform.localPosition = Vector2.zero;
+
+                barrier.SetActive(false);
+
+                isFinishing = false;
+            }
+
+            Vector2 clampedPosition = rb.transform.position;
+            clampedPosition.x = Mathf.Clamp(clampedPosition.x, topLeftBound.x, bottomRightBound.x);
+            clampedPosition.y = Mathf.Clamp(clampedPosition.y, bottomRightBound.y, topLeftBound.y);
+            rb.transform.position = clampedPosition;
+
+            finishTimer += Time.deltaTime;
         }
     }
 
-    public void MyMouseDown() {
-        isRolling = true;
+    public void MouseDownFunc() {
+        if (!isFinishing) {
+            isRolling = true;
+            animateTimer = animateInterval;
+        }
     }
 
-    public void MyMouseUp() {
+    public void MouseUpFunc() {
         isRolling = false;
+        isFinishing = true;
+        finishTimer = 0;
+
+        roll = Random.Range(1, 7);
+        spriteRenderer.sprite = sprites[roll - 1];
+        rb.velocity *= releaseMultiplier;
+
+        barrier.SetActive(true);
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(topLeftBound, 0.5f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(bottomRightBound, 0.5f);
     }
 }
