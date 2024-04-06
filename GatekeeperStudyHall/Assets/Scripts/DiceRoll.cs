f64d466d4afed080eab90892c1efc39999a562bf
Gunnar Dahl <gunnardahl4@gmail.com>
2024-04-06 14:31:09 -0500

    reorganized DiceRoll script
    Added comments to make it more clear how the dice roll works, factored out functions `UpdateHeldDice` and `UpdateSlidingDice`

--- GatekeeperStudyHall/Assets/Scripts/DiceRoll.cs
+++ GatekeeperStudyHall/Assets/Scripts/DiceRoll.cs
@@ -1,55 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class DiceRoll : MonoBehaviour
 {
-    bool isRolling = false;
-    bool isFinishing = false;
+    bool isHeld = false;
+    bool isSliding = false;
 
-    [SerializeField]
-    float maxAngularVel = 720f;
-    [SerializeField]
-    float minAngularVel = 360f;
+    // angular velocities the dice is set to while held
+    [SerializeField] float maxAngularVel = 720f;
+    [SerializeField] float minAngularVel = 360f;
 
-    [SerializeField]
-    Sprite[] sprites;
+    [SerializeField] Sprite[] sprites; // the 6 dice faces
 
     SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
 
-    float animateTimer = 0f;
-    [SerializeField, Range(0f, 1f)]
-    float animateInterval = 0.1f;
+    float shakeTimer = 0f;
+    [SerializeField, Range(0f, 1f)] float shakeInterval = 0.1f;
 
-    float finishTimer = 0f;
-    [SerializeField, Range(0f, 2f)]
-    float finishDuration = 0.75f;
+    float slideTimer = 0f;
+    [SerializeField, Range(0f, 2f)] float slideDuration = 0.75f;
 
-    [SerializeField, Range(0, 0.5f)]
-    float frictionFactor = 0.03f;
+    [SerializeField, Range(0, 0.2f)] float frictionFactor = 0.03f;
 
-    [SerializeField]
-    float shakeSpeed = 10f;
-    [SerializeField]
-    float shakeRadius = 1f;
-    [SerializeField]
-    float releaseMultiplier = 5f;
+    [SerializeField] float shakeSpeed = 10f;
+    [SerializeField] float shakeRadius = 1f;
+    [SerializeField] float releaseMultiplier = 5f;
 
-    [SerializeField]
-    Vector2 topLeftBound;
-    [SerializeField]
-    Vector2 bottomRightBound;
+    // the dice is restricted between these while sliding
+    [SerializeField] Vector2 topLeftBound;
+    [SerializeField] Vector2 bottomRightBound;
 
-    [SerializeField]
-    GameObject barrier;
+    [SerializeField] GameObject barrier;
 
     int roll = -1;
+    public UnityEvent<int> endEvent; // functions to be called after the dice is done rolling
 
-    private void Start() {
+
+    void Start() {
         if (sprites.Length != 6) {
             Debug.LogError($"There should be 6 sprites in DiceRoll array (actual: {sprites.Length})");
         }
+
+        // temporary debug message
+        endEvent.AddListener(x => Debug.Log($"You rolled a {roll}."));
 
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         rb = GetComponentInChildren<Rigidbody2D>();
@@ -57,86 +57,140 @@ public class DiceRoll : MonoBehaviour
     }
 
     void Update() {
+        if (isHeld) {
+            UpdateHeldDice();
+        } 
 
-        if (isRolling) {
-
-            Vector3 mousePosition = Input.mousePosition;
-            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-            mousePosition.z = transform.position.z;
-            transform.position = mousePosition;
-
-            while (animateTimer >= animateInterval) {
-                spriteRenderer.sprite = sprites[Random.Range(0, 6)];
-
-                float unitCircleAngle = Random.Range(0f, 360f);
-                Vector3 unitCircleVec = new Vector3(Mathf.Cos(unitCircleAngle), Mathf.Sin(unitCircleAngle), 0);
-                Vector3 targetPos = unitCircleVec * shakeRadius;
-                Vector3 newVel = targetPos - rb.transform.localPosition;
-                newVel.z = 0;
-                rb.velocity = newVel.normalized * shakeSpeed;
-
-                float angVel = Random.Range(minAngularVel, maxAngularVel);
-                if (Random.value <= 0.5f) {
-                    angVel = -angVel;
-                }
-                rb.angularVelocity = angVel;
-
-                animateTimer -= animateInterval;
-            }
-
-            animateTimer += Time.deltaTime;
-        }
-
-        else if (isFinishing) {
-
-            rb.angularVelocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
-            rb.velocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
-
-            if (finishTimer >= finishDuration) {
-                rb.angularVelocity = 0;
-                rb.velocity = Vector2.zero;
-                Debug.Log($"You rolled a {roll}.");
-
-                transform.position = rb.transform.position;
-                rb.transform.localPosition = Vector2.zero;
-
-                barrier.SetActive(false);
-
-                isFinishing = false;
-            }
-
-            Vector2 clampedPosition = rb.transform.position;
-            clampedPosition.x = Mathf.Clamp(clampedPosition.x, topLeftBound.x, bottomRightBound.x);
-            clampedPosition.y = Mathf.Clamp(clampedPosition.y, bottomRightBound.y, topLeftBound.y);
-            rb.transform.position = clampedPosition;
-
-            finishTimer += Time.deltaTime;
+        else if (isSliding) {
+            UpdateSlidingDice();
         }
     }
 
+
+    // called whenever the dice is clicked on
     public void MouseDownFunc() {
-        if (!isFinishing) {
-            isRolling = true;
-            animateTimer = animateInterval;
+        // we will likely have to change the condition here to account for
+        // all the times you are and aren't allowed to pick up the dice
+        if (!isSliding) {
+            isHeld = true;
+            shakeTimer = shakeInterval;
         }
     }
 
-    public void MouseUpFunc() {
-        isRolling = false;
-        isFinishing = true;
-        finishTimer = 0;
 
+    private void UpdateHeldDice() {
+
+        // move the dice base to the mouse's x and y position
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = transform.position.z;
+        transform.position = mousePosition;
+
+        // shake the dice once every `shakeInterval` seconds
+        if (shakeTimer >= shakeInterval) {
+
+            // change the face to show a random number
+            spriteRenderer.sprite = sprites[Random.Range(0, 6)];
+
+            // get a random point `shakeRadius` units away from the mouse,
+            // then start moving the dice sprite towards it
+            float unitCircleAngle = Random.Range(0f, 360f);
+            Vector3 unitCircleVec = new Vector3(Mathf.Cos(unitCircleAngle), Mathf.Sin(unitCircleAngle), 0);
+            Vector3 targetPos = unitCircleVec * shakeRadius;
+            Vector3 newVel = targetPos - rb.transform.localPosition;
+            newVel.z = 0;
+            rb.velocity = newVel.normalized * shakeSpeed;
+
+            // randomize angular velocity
+            float angVel = Random.Range(minAngularVel, maxAngularVel);
+            if (Random.value <= 0.5f) {
+                angVel = -angVel;
+            }
+            rb.angularVelocity = angVel;
+
+            // un-tick the timer
+            shakeTimer -= shakeInterval;
+        }
+
+        // tick the timer
+        shakeTimer += Time.deltaTime;
+    }
+
+
+    // called whenever the dice stops being clicked on
+    public void MouseUpFunc() {
+        if (!isHeld) {
+            return;
+        }
+
+        isHeld = false;
+        isSliding = true;
+        slideTimer = 0;
+
+        // determines the roll that this dice will get
         roll = Random.Range(1, 7);
         spriteRenderer.sprite = sprites[roll - 1];
+
+        // gives the dice a boost
         rb.velocity *= releaseMultiplier;
 
+        // restrict the dice's position to inside the screen bounds
+        ClampDice();
         barrier.SetActive(true);
     }
 
-    void OnDrawGizmosSelected() {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(topLeftBound, 0.5f);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(bottomRightBound, 0.5f);
+
+    private void UpdateSlidingDice() {
+        // apply friction
+        rb.angularVelocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
+        rb.velocity *= Mathf.Pow(frictionFactor, Time.deltaTime);
+
+        ClampDice();
+
+        if (slideTimer >= slideDuration) {
+            // finish sliding
+
+            rb.angularVelocity = 0;
+            rb.velocity = Vector2.zero;
+
+            // shift this transform to be directly under the dice sprite's transform
+            transform.position = rb.transform.position;
+            rb.transform.localPosition = Vector2.zero;
+
+            barrier.SetActive(false);
+
+            isSliding = false;
+
+            // call some function after the dice is done rolling
+            endEvent.Invoke(roll);
+        }
+
+        slideTimer += Time.deltaTime;
     }
+
+
+    // restricts the dice's position so that it is between topLeftBound and bottomRightBound
+    private void ClampDice() {
+        Vector2 clampedPosition = rb.transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, topLeftBound.x, bottomRightBound.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, bottomRightBound.y, topLeftBound.y);
+        rb.transform.position = clampedPosition;
+    }
+
+
+
+    // some debug code to show where the dice teleport bounds are when this gameobject is selected
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected() {
+        float gizmoRadius = 0.25f;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(topLeftBound, gizmoRadius);
+        Handles.Label(topLeftBound + Vector2.right * gizmoRadius, "Upper Left Bound");
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(bottomRightBound, gizmoRadius);
+        Handles.Label(bottomRightBound + Vector2.right * gizmoRadius, "Bottom Right Bound");
+    }
+#endif
 }
