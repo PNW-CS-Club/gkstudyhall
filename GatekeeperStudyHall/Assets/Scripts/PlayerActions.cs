729836d0f7897461bc242c4800fe56d1842992b7
Gunnar Dahl <gunnardahl4@gmail.com>
2024-04-12 14:58:15 -0500

    Moved & removed a lot of assets
    Moved the hype_board image into the image folder, re-made the Battling SO, re-deleted PlyerActions, removed the TextMeshPro/Examples folder, made the Dice Barrier into a prefab, made the StartScene use screen space: camera

--- GatekeeperStudyHall/Assets/Scripts/PlayerActions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerActions : MonoBehaviour
-{
-    public int DiceRoll(){ return Random.Range(1,6);}
-
-}
