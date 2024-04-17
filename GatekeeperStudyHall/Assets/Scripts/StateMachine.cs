df5fcb0523e9605d06ad09a50d7e669ce26535ef
Gunnar Dahl <gunnardahl4@gmail.com>
2024-04-17 16:55:13 -0500

    Split up state tester
    Removed a lot of stuff and split the StateTester file into States, StateManager, and StateTester

---
+++ GatekeeperStudyHall/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class StateMachine
+{
+    List<PlayerSO> players;
+
+    public TraitRollState traitRollState;
+
+    IState currentState;
+
+    public StateMachine(List<PlayerSO> players) {
+        this.players = players;
+
+        traitRollState = new(players);
+    }
+
+    public void Initialize(IState state) {
+        currentState = state;
+        currentState.Enter();
+    }
+
+    public void TransitionTo(IState state) {
+        currentState.Exit();
+        currentState = state;
+        currentState.Enter();
+    }
+
+    public void Update() {
+        currentState.Update();
+    }
+}
+
