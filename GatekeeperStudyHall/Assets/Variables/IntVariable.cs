22fc787f008487793b788b3dd2f317aab20d840e
Gunnar Dahl <gunnardahl4@gmail.com>
2024-04-22 00:41:34 -0500

    Implemented IntVariable
    Created the IntVariable Scriptable Object and the IntReference class that lets you choose between an int and an IntVariable in the editor
    
    Note: none of this code is my ideas, I took inspiration and a chunk of code from a Scriptable Object demo at Unite Austin

---
+++ GatekeeperStudyHall/Assets/Variables/IntVariable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New_IntVariable", menuName = "Variables/IntVariable")]
+public class IntVariable : ScriptableObject
+{
+    public int value;
+}
+
+
