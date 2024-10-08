dc2e5716a2cf3e1e34d3482fe06afbafc590c40d
Gunnar Dahl <gunnardahl4@gmail.com>
2024-10-08 14:51:44 -0500

    Health bar shows shield
    The health bar now shows whether the player has a shield or not. If they do it turns blue and gets a shield icon.

--- GatekeeperStudyHall/Assets/Scripts/VisualScripts/HealthDisplay.cs
+++ GatekeeperStudyHall/Assets/Scripts/VisualScripts/HealthDisplay.cs
@@ -6,16 +6,25 @@ using UnityEngine.UI;
 public class HealthDisplay : MonoBehaviour
 {
     [SerializeField] PlayerSO player;
+    [SerializeField] Color shieldColor;
 
+    Color defaultColor;
     float initialWidth;
     RectTransform slider;
     TMPro.TMP_Text healthCounter;
 
+    bool hadStockadeLastUpdate = false;
+
     void Start() 
     {
         slider = transform.GetChild(1).GetComponent<RectTransform>();
+        defaultColor = slider.GetComponent<Image>().color;
         initialWidth = slider.sizeDelta.x;
+
         healthCounter = transform.GetComponentInChildren<TMPro.TMP_Text>();
+
+        transform.GetChild(2).GetChild(0).gameObject.SetActive(true);
+        transform.GetChild(2).GetChild(1).gameObject.SetActive(false);
     }
 
     void Update()
@@ -23,5 +32,25 @@ public class HealthDisplay : MonoBehaviour
         float healthRatio = Mathf.Clamp(player.health, 0, PlayerSO.MAX_HEALTH) / (float)PlayerSO.MAX_HEALTH;
         slider.sizeDelta = new Vector2(initialWidth * healthRatio, slider.sizeDelta.y);
         healthCounter.text = player.health.ToString();
+
+        if (player.hasStockade != hadStockadeLastUpdate) 
+        {
+            if (player.hasStockade) 
+            {
+                slider.GetComponent<Image>().color = shieldColor;
+                // turns on the shield and off the circle
+                transform.GetChild(2).GetChild(0).gameObject.SetActive(false);
+                transform.GetChild(2).GetChild(1).gameObject.SetActive(true);
+            }
+            else 
+            {
+                slider.GetComponent<Image>().color = defaultColor;
+                // turns on the circle and off the shield
+                transform.GetChild(2).GetChild(0).gameObject.SetActive(true);
+                transform.GetChild(2).GetChild(1).gameObject.SetActive(false);
+            }
+
+            hadStockadeLastUpdate = player.hasStockade;
+        }
     }
 }
