5d61e13a8cd8f5cec73ccd86bcf00a246c748cdc
Gunnar Dahl <gunnardahl4@gmail.com>
2024-10-29 17:56:38 -0500

    created script that shows panel with all players

---
+++ GatekeeperStudyHall/Assets/Scripts/PlayerSelection.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerSelection : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] PlayerListSO playerListSO;
+    [SerializeField] GameObject cardDisplayPrefab;
+    [SerializeField, Min(0)] float margin;
+    [SerializeField] Vector2 padding;
+
+    bool isInitialized = false;
+    List<PlayerSO> playerList;
+    RectTransform panel;
+    List<GameObject> displayObjects;
+    Vector2 cardSize;
+
+
+    void OnEnable()
+    {
+        if (!isInitialized)
+        {
+            playerList = playerListSO.list;
+            panel = transform.GetChild(0).GetComponent<RectTransform>();
+            displayObjects = new();
+
+            for (int i = 0; i < playerList.Count; i++)
+            {
+                displayObjects.Add(Instantiate(cardDisplayPrefab, panel));
+            }
+
+            cardSize = cardDisplayPrefab.GetComponent<RectTransform>().sizeDelta;
+
+            isInitialized = true;
+        }
+    }
+
+
+    public void ShowExcluding(int excludedCardIndex)
+    {
+        gameObject.SetActive(true);
+        displayObjects[playerList.Count - 1].gameObject.SetActive(false);
+
+        int playerIndex = 0;
+        int displayIndex = 0;
+
+        while (playerIndex < playerList.Count)
+        {
+            if (playerIndex != excludedCardIndex)
+            {
+                displayObjects[displayIndex].GetComponent<CardDisplay>().ChangeCardData(playerList[playerIndex].card);
+                displayIndex++;
+            }
+            playerIndex++;
+        }
+
+        Reposition(playerList.Count - 1);
+    }
+
+
+    public void ShowAll()
+    {
+        gameObject.SetActive(true);
+        displayObjects[playerList.Count - 1].SetActive(true);
+
+        for (int i = 0; i < playerList.Count; i++)
+        {
+            displayObjects[i].GetComponent<CardDisplay>().ChangeCardData(playerList[i].card);
+        }
+
+        Reposition(playerList.Count);
+    }
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        gameObject.SetActive(false);
+    }
+
+
+    void Reposition(int numDisplays)
+    {
+        float xOffset = padding.x;
+        for (int i = 0; i < numDisplays; i++)
+        {
+            var rect = displayObjects[i].GetComponent<RectTransform>();
+            rect.anchorMin = new Vector2(0f, 0.5f);
+            rect.anchorMax = new Vector2(0f, 0.5f);
+            rect.pivot = new Vector2(0f, 0.5f);
+            rect.anchoredPosition = new Vector2(xOffset, 0);
+            xOffset += cardSize.x + margin;
+        }
+
+        panel.sizeDelta = new Vector2(xOffset - margin + padding.x, padding.y * 2 + cardSize.y);
+    }
+}
--- GatekeeperStudyHall/Assets/Scripts/VisualScripts/CardMagnifier.cs
+++ GatekeeperStudyHall/Assets/Scripts/VisualScripts/CardMagnifier.cs
@@ -7,15 +7,20 @@ public class CardMagnifier : MonoBehaviour, IPointerClickHandler
 {
     CardDisplay magnifiedDisplay;
 
-    void Start() {
-        magnifiedDisplay = transform.GetChild(0).GetComponent<CardDisplay>();
-        magnifiedDisplay.transform.localScale = Vector3.one * 2.0f;
-        gameObject.SetActive(false);
+    bool isInitialized = false;
+
+    void OnEnable() {
+        if (!isInitialized) {
+            magnifiedDisplay = transform.GetChild(0).GetComponent<CardDisplay>();
+            magnifiedDisplay.transform.localScale = Vector3.one * 2.0f;
+            
+            isInitialized = true;
+        }
     }
 
     public void Show(CardSO cardData) {
-        magnifiedDisplay.ChangeCardData(cardData);
         gameObject.SetActive(true);
+        magnifiedDisplay.ChangeCardData(cardData);
     }
 
     public void OnPointerClick(PointerEventData eventData) {
