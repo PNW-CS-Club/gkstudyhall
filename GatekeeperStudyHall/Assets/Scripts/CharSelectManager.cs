16b2a485cb6eaf056b6361f48183c3d22f360073
Marco-Minetti <99402068+Marco-Minetti@users.noreply.github.com>
2024-10-29 18:08:42 -0500

    Add the visual mecanism to change player

--- GatekeeperStudyHall/Assets/Scripts/CharSelectManager.cs
+++ GatekeeperStudyHall/Assets/Scripts/CharSelectManager.cs
@@ -1,13 +1,16 @@
-using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 
 public class CharSelectManager : MonoBehaviour
 {
+    
     [SerializeField] PlayerListSO playerListObject;
     List<PlayerSO> playerList; // refers to list in playerListObject
-
+    
     [SerializeField] PlayerSO player1;
+
+
     // Awake is called as soon as the object is created
     private void Start() 
     {
