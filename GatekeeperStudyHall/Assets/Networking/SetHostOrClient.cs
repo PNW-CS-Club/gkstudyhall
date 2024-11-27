b9ab5c3361314eebbe489addfb7ef8f3efa67e2f
Gunnar Dahl <gunnardahl4@gmail.com>
2024-11-26 18:28:04 -0600

    ip textbox

--- GatekeeperStudyHall/Assets/Networking/SetHostOrClient.cs
+++ GatekeeperStudyHall/Assets/Networking/SetHostOrClient.cs
@@ -1,11 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class SetHostOrClient : MonoBehaviour
 {
     NetworkManager m_NetworkManager;
+    
+    [SerializeField] TextMeshProUGUI ipAddressText;
+    [SerializeField] TMP_InputField ip;
+
+    [SerializeField] string ipAddress;
+    [SerializeField] UnityTransport transport;
+    
+    /* Gets the Ip Address of your connected network and
+    shows on the screen in order to let other players join
+    by inputing that Ip in the input field */
+    // ONLY FOR HOST SIDE 
+    public string GetLocalIPAddress() {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList) {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) {
+            	ipAddressText.text = ip.ToString();
+            	ipAddress = ip.ToString();
+            	return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
+    }
+    
+    /* Sets the Ip Address of the Connection Data in Unity Transport
+    to the Ip Address which was input in the Input Field */
+    // ONLY FOR CLIENT SIDE
+    public void SetIpAddress()
+    {
+        ipAddress = ip.text;
+        transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = ipAddress;
+    }
 
     void Awake()
     {
@@ -19,11 +55,13 @@ public class SetHostOrClient : MonoBehaviour
     
     public void SetHost() 
     {
+        GetLocalIPAddress();
         m_NetworkManager.StartHost();
     }
 
     public void SetClient()
     {
+        SetIpAddress();
         m_NetworkManager.StartClient();
     }
 
