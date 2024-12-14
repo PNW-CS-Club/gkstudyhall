6c2dc603d83deea7106595ddb2b5e79dbdc56a8d
Gunnar Dahl <gunnardahl4@gmail.com>
2024-12-14 13:25:23 -0600

    ping pong test

---
+++ GatekeeperStudyHall/Assets/Scripts/TestRpcs.cs
@@ -0,0 +1,32 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class TestRpcs : NetworkBehaviour
+{
+    int pingCounter = 0;
+    
+    [ServerRpc]
+    public void Ping_ServerRpc(int pingCount)
+    {
+        // Server -> Clients because PongRpc sends to NotServer
+        // Note: This will send to all clients.
+        // Sending to the specific client that requested the pong will be discussed in the next section.
+        Pong_ClientRpc(pingCount, "PONG!");
+    }
+
+    [ClientRpc]
+    void Pong_ClientRpc(int pingCount, string message)
+    {
+        Debug.Log($"Received pong from server for ping {pingCount} and message {message}");
+    }
+    
+    void Update()
+    {
+        if (IsClient && Input.GetKeyDown(KeyCode.P))
+        {
+            // Client -> Server because PingRpc sends to Server
+            Ping_ServerRpc(pingCounter);
+            pingCounter++;
+        }
+    }
+}
