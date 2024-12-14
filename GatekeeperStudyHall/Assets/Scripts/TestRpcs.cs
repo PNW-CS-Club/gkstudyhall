2890a1f891ca4a48c152c9c3defeb5ee931ec670
Gunnar Dahl <gunnardahl4@gmail.com>
2024-12-14 13:55:18 -0600

    ping pong test 2

--- GatekeeperStudyHall/Assets/Scripts/TestRpcs.cs
+++ GatekeeperStudyHall/Assets/Scripts/TestRpcs.cs
@@ -6,26 +6,26 @@ public class TestRpcs : NetworkBehaviour
     int pingCounter = 0;
     
     [Rpc(SendTo.Everyone)]
-    public void Ping_ServerRpc(int pingCount)
+    public void Ping_Rpc(int pingCount)
     {
         // Server -> Clients because PongRpc sends to NotServer
         // Note: This will send to all clients.
         // Sending to the specific client that requested the pong will be discussed in the next section.
-        Pong_ClientRpc(pingCount, "PONG!");
+        Pong_Rpc(pingCount, "PONG!");
     }
 
     [Rpc(SendTo.Server)]
-    void Pong_ClientRpc(int pingCount, string message)
+    void Pong_Rpc(int pingCount, string message)
     {
         Debug.Log($"Received pong from server for ping {pingCount} and message {message}");
     }
     
     void Update()
     {
-        if (IsClient && Input.GetKeyDown(KeyCode.P))
+        if (IsClient && IsOwner && Input.GetKeyDown(KeyCode.P))
         {
             // Client -> Server because PingRpc sends to Server
-            Ping_ServerRpc(pingCounter);
+            Ping_Rpc(pingCounter);
             pingCounter++;
         }
     }
