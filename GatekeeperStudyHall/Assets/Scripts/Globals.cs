687148edd1ac82ecf544b89bd1286627692f5c7e
Gunnar Dahl <gunnardahl4@gmail.com>
2024-12-22 19:22:58 -0600

    update globals comments

--- GatekeeperStudyHall/Assets/Scripts/Globals.cs
+++ GatekeeperStudyHall/Assets/Scripts/Globals.cs
@@ -1,24 +1,17 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
 /// Holds references to any objects we want easy access to. 
 /// Cannot be instantiated; instead the references are stored in static fields.
-/// </summary>
 public static class Globals
 {
-    /// <summary>
     /// The gate that the current player has chosen to attack. 
     /// Holds <c>null</c> if the player has not yet chosen a gate or has already attacked a gate.
-    /// </summary>
     public static GateSO selectedGate = null;
-
-    /// <summary>
+    
     ///  A structure of data relevant to battles.
     ///  <c>data</c> needs to be init'd so that it's technically a valid object.
-    /// </summary>
     public struct BattleData {
         public static void Reset() {
             data = new();
@@ -31,10 +24,10 @@ public static class Globals
         public static int mult;
     }
 
-    /// <summary>
     /// The number of players still alive 
-    /// </summary>
     public static int playersAlive = -1;
 
+    /// The player who won the game
+    /// (only valid after a player has won and before the next game starts)
     public static PlayerSO winningPlayer;
 }
