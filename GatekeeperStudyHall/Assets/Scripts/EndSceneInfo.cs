4d7c76efd58a77c3f013113b89da65f40984874b
Julian <sahaguj@pnw.edu>
2024-12-28 17:12:50 -0600

    Add Placeholders for 4 Players
    Note that this does not currently adjust to the amount of players in the game and will throw errors with less than 4 players.

--- GatekeeperStudyHall/Assets/Scripts/EndSceneInfo.cs
+++ GatekeeperStudyHall/Assets/Scripts/EndSceneInfo.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class EndSceneInfo : MonoBehaviour
 {
+    [SerializeField] PlayerListSO playerListSO;
     [SerializeField] CardDisplay cardDisplay1;
     [SerializeField] CardDisplay cardDisplay2;
     [SerializeField] CardDisplay cardDisplay3;
     [SerializeField] CardDisplay cardDisplay4;
+
+    [SerializeField] GameObject statDisplay1;
+    [SerializeField] GameObject statDisplay2;
+    [SerializeField] GameObject statDisplay3;
+    [SerializeField] GameObject statDisplay4;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +24,25 @@ public class EndSceneInfo : MonoBehaviour
     }
 
     void Awake(){
+        List<PlayerSO> playerList = playerListSO.list;
+
         cardDisplay1.cardData = Globals.winningPlayer.card;
+
+        // set the rest of the card displays to the other players
+        
+        cardDisplay2.cardData = playerList[1].card;
+        cardDisplay3.cardData = playerList[2].card;
+        cardDisplay4.cardData = playerList[3].card;
+        
+
+        statDisplay1.GetComponent<TMP_Text>().text = 
+            $"Total Damage Dealt to Other Players: {Globals.winningPlayer.totalDamageToOtherPlayers}\n" +
+            $"Total Damage Dealt to Gates: {Globals.winningPlayer.totalDamageToGates}\n" +
+            $"Total Damage Dealt to Gatekeeper: {Globals.winningPlayer.totalDamageToGatekeeper}\n" +
+            $"Total Amount Healed: {Globals.winningPlayer.totalAmountHealed}\n" +
+            $"Total Damage Taken: {Globals.winningPlayer.totalDamageTaken}\n" +
+            $"Total Stockade Collected: {Globals.winningPlayer.totalStockade}\n" +
+            $"Battles Started: {Globals.winningPlayer.battlesStarted}\n";
     }
 
     // Update is called once per frame
