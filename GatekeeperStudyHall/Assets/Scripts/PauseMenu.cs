d43cfd2657e6c690c7f146b78f20fdfd58b5679e
Daniel-A27 <dasahagun27@gmail.com>
2025-01-29 16:02:43 -0600

    Created Pause Menu
    If you click escape in the GK scene it will open up a pause menu with three buttons including resume, menu, and quit. all the buttons work.

---
+++ GatekeeperStudyHall/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(GameIsPaused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        //Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        //Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    public void LoadMenu()
+    {
+        //Time.timeScale = 1f;
+        SceneManager.LoadScene("CharSelectScene");
+    }
+    public void QuitGame()
+    {
+    // save any game data here
+    #if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
+    }
+}
