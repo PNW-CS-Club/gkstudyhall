0446bdaa2f3a8976ea77562863833e77290047c8
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-02-12 14:47:15 -0600

    Code tweaks for consistency

--- GatekeeperStudyHall/Assets/Scripts/ButtonScripts/CharSelectMenuOnclick.cs
+++ GatekeeperStudyHall/Assets/Scripts/ButtonScripts/CharSelectMenuOnclick.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,11 +5,13 @@ using UnityEngine.SceneManagement;
 public class CharSelectMenuOnClick : MonoBehaviour
 {
     [SerializeField] PlayerListSO playerListObject;
-    [SerializeField] PlayerSO p4;
+    [SerializeField] PlayerListSO backupListObject;
     List<PlayerSO> players;
     public CardSO randomCard;
     public CardSO clearCard;
 
+    [SerializeField] PlayerSO p3;
+    [SerializeField] PlayerSO p4;
     [SerializeField] GameObject Card3;
     [SerializeField] GameObject Card4;
     
@@ -32,10 +33,10 @@ public class CharSelectMenuOnClick : MonoBehaviour
         }
 
         //reset players
-        for(int i = 0; i < players.Count; i++)
+        foreach (var player in players)
         {
-            players[i].GameReset();
-            players[i].ResetEffects();
+            player.GameReset();
+            player.ResetEffects();
         }
         AsyncOperation _ = SceneManager.LoadSceneAsync("GkScene");
 
@@ -52,43 +53,48 @@ public class CharSelectMenuOnClick : MonoBehaviour
         AsyncOperation _ = SceneManager.LoadSceneAsync("StartScene");
     }
 
-    public void AddPlayer(PlayerSO p3)
+    ///This function is for the AddPlayerButton.
+    ///When the button is clicked, the player should be added to the list of players.
+    public void AddPlayer()
     {
-        //This function will be for the AddPlayerButton
-        //When the button is clicked, the player should be added to the list of players
-        if(players.Count < 4) {
-            if(players.Count < 3) {
+        switch (players.Count)
+        {
+            case <= 2:
                 players.Add(p3);
-                Card3.SetActive(true); 
-            }
-            else {
+                Card3.SetActive(true);
+                return;
+            case 3:
                 players.Add(p4);
-                Card4.SetActive(true); 
-            }
+                Card4.SetActive(true);
+                return;
+            case >= 4:
+                return;
         }
-
     }
 
+    ///This function will be for the AddBotButton.
+    ///When the button is clicked, an instance of the Bot class should be made and added to the list of players.
     public void AddBot()
     {
-        //This function will be for the AddBotButton
-        //When the button is clicked, an instance of the Bot class should be made and added to the list of players
     
     }
 
-    public void RemovePlayer(PlayerSO p3)
+    ///This function is for the RemovePlayerButton on both Bots and Players.
+    ///When the button is clicked, remove the Player/Bot from the list of players and make the card available again.
+    public void RemovePlayer()
     {
-        //This function will be for the RemovePLayerButton on both Bots and Players
-        //When the button is clicked, remove the Player/Bot from the list of players and make the card available again
-        if(players.Count > 2) {
-            if(players.Count > 3) {
-                players.Remove(p4);
-                Card4.SetActive(false); 
-            }
-            else {
+        switch (players.Count)
+        {
+            case <= 2:
+                return;
+            case 3:
                 players.Remove(p3);
-                Card3.SetActive(false); 
-            }    
+                Card3.SetActive(false);
+                return;
+            case >= 4:
+                players.Remove(p4);
+                Card4.SetActive(false);
+                return;
         }
     }
 }
