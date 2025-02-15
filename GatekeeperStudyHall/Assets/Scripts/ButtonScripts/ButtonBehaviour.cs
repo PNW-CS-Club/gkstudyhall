d86494c0b09a108ca7839f3fd0028a1169ca3f49
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-02-15 16:14:00 -0600

    Better organized buttons, hover highlight script

--- GatekeeperStudyHall/Assets/Scripts/ButtonScripts/ButtonBehaviour.cs
+++ GatekeeperStudyHall/Assets/Scripts/ButtonScripts/ButtonBehaviour.cs
@@ -2,43 +2,69 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ButtonBehaviour : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+public class ButtonBehaviour : MonoBehaviour, 
+    IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
 {
     public UnityEvent OnClick;
     
+    [SerializeField] Material defaultMaterial;
+    [SerializeField] Material hoverMaterial;
+    [SerializeField] Material pressMaterial;
+    [SerializeField] List<Image> buttonImages;
+    
     bool hovering = false;
+    bool pressing = false;
 
     void OnDisable()
     {
-        if (hovering) EndHover();
+        pressing = false;
+        hovering = false;
+        UpdateMaterials();
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        OnClick.Invoke();
-    }
+    public void OnPointerEnter(PointerEventData eventData) => StartHover();
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        StartHover();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        EndHover();
-    }
+    public void OnPointerExit(PointerEventData eventData) => EndHover();
+    public void OnPointerDown(PointerEventData eventData) => StartPress();
+    public void OnPointerUp(PointerEventData eventData) => EndPress();
+    public void OnPointerClick(PointerEventData eventData) => OnClick.Invoke();
 
 
     void StartHover()
     {
         hovering = true;
-        //print("StartHover");
+        UpdateMaterials();
     }
 
     void EndHover()
     {
         hovering = false;
-        //print("EndHover");
+        UpdateMaterials();
+    }
+
+    void StartPress()
+    {
+        pressing = true;
+        UpdateMaterials();
+    }
+
+    void EndPress()
+    {
+        pressing = false;
+        UpdateMaterials();
+    }
+
+    void UpdateMaterials()
+    {
+        Material mat = defaultMaterial;
+        if (pressing)
+            mat = pressMaterial;
+        else if (hovering)
+            mat = hoverMaterial;
+        
+        foreach (Image image in buttonImages) 
+            image.material = mat;
     }
 }
