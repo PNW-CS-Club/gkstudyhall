49bbe4cc95ca74d3d8066d4cb97ba67c2b1054ed
Gunnar Dahl <73252935+gadahl@users.noreply.github.com>
2025-02-27 21:54:20 -0600

    a little bit of layout & formatting
    The clock is anchored in the corner now
    Removed leading zeroes from leading hours/minutes
    (04:14 -> 4:14, 06:07:08 -> 6:07:08)

--- GatekeeperStudyHall/Assets/Scripts/VisualScripts/ClockDisplay.cs
+++ GatekeeperStudyHall/Assets/Scripts/VisualScripts/ClockDisplay.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 
@@ -7,23 +6,21 @@ public class ClockDisplay : MonoBehaviour {
 
     private float startTime = 0f;
 
-    // Start is called before the first frame update
     void Start() {
         this.startTime = Time.time;
     }
 
-    // Update is called once per frame
     void Update() {
-        float timeElapsed = ( Time.time - this.startTime );
+        float timeElapsed = Time.time - this.startTime;
 
         int numHours = ( int )timeElapsed / ( 60 * 60 );
         int numMins = ( int )( timeElapsed / 60 ) % 60;
         int numSecs = ( int )( timeElapsed % 60 );
 
         if ( timeElapsed >= 60f * 60f ) {
-            textObj.text = String.Format( "{0:D2}:{1:D2}:{2:D2}", numHours, numMins, numSecs );
+            textObj.text = $"{numHours}:{numMins:D2}:{numSecs:D2}";
         } else {
-            textObj.text = String.Format( "{0:D2}:{1:D2}", numMins, numSecs );
+            textObj.text = $"{numMins}:{numSecs:D2}";
         }
     }
 
