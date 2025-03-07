a81d766b1f0583581e677b807eb5be6dcdc86827
Daniel-A27 <dasahagun27@gmail.com>
2025-03-06 20:35:37 -0600

    142 add soundeffects (#168)
    * SoundEffects
    
    Add SoundEffects
    
    * Fixed something
    
    fixed an error
    
    * Sliders now work
    
    The sliders can now actually change the volume
    
    * remove unneeded text from gkscene
    
    * delete duplicate text from merge
    
    ---------
    
    Co-authored-by: Julian <sahaguj@pnw.edu>

---
+++ GatekeeperStudyHall/Assets/Scripts/SoundScripts/Sound.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3)]
+    public float pitch;
+    [HideInInspector]
+    public AudioSource source;
+}
