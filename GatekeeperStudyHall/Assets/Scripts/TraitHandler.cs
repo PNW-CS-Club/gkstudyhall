8368faef7f05abf36965ccc8f16802eb3be42685
ejvogt5 <vogte@pnw.edu>
2025-03-18 15:10:05 -0500

    fix deal 3 damage

--- GatekeeperStudyHall/Assets/Scripts/TraitHandler.cs
+++ GatekeeperStudyHall/Assets/Scripts/TraitHandler.cs
@@ -84,7 +84,7 @@ public class TraitHandler : MonoBehaviour
                     gameManager.currentState = State.ChoosingGate;
                 };
 
-                return State.ChoosingGate;
+                return State.ChoosingPlayer;
 
             case Trait.minus2GateKeeper:
                 //Selected gate health - 2
